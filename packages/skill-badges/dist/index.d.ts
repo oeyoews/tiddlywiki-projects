626c243fa6280d79d9df0b0fe65b80d017ad2b0a
oeyoews <jyao4783@gmail.com>
2023-12-21 23:34:52 +0800

    update types

--- packages/skill-badges/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-type BadgeNames = 'ArchLinux' | 'NuxtJS' | 'Vite' | 'Vim' | 'Neovim' | 'Chrome' | 'NPM' | 'Linux' | 'HTML5' | 'JavaScript' | 'CSS3' | 'NodeJS' | 'React' | 'Vue' | 'TypeScript' | 'TailwindCSS' | 'NextJS' | 'TiddlyWiki5' | 'Angular' | 'PHP' | 'Ruby' | 'Python' | 'Java' | 'CSharp' | 'Golang' | 'Rust' | 'Docker' | 'Kubernetes' | 'Vercel';
-type Badges = Record<BadgeNames, string>;
-declare const badges: Badges;
-export default badges;
-export type { BadgeNames };
